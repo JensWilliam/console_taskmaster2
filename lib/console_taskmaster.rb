9636270e4baa9aa6292e7b52c4a1ce5846d677c2
Nilas <nilasandresen@gmail.com>
2024-12-09 20:28:24 +0100

    la til gems: tty prompt og colorize

--- lib/console_taskmaster.rb
+++ lib/console_taskmaster.rb
@@ -1,150 +1,164 @@
 require "json"
+require "colorize"
+require "tty-prompt"
+
+def load_json(path)
+    JSON.parse(File.read(path))
+rescue Errno::ENOENT
+    puts "File not found: #{path}"
+    exit
+rescue JSON::ParserError
+    puts "Invalid JSON in file: #{path}"
+    exit
+end
+
+def write_json(path, data)
+    File.write(path, JSON.dump(data))
+rescue IOError
+    puts "Error writing to file: #{path}"
+    exit
+end
+
+
+
+$tasks = load_json("json_tabeller/tasks.json")
+$users = load_json("json_tabeller/bruker.json")
+$task_records = load_json("json_tabeller/task_record.json")
+
+def clear_screen
+    system('clear')
+end
+
+
+
+
+
+
+
+
 
 def tasks()
-    path_a = "json_tabeller/tasks.json"
-    jsonString_a = File.read(path_a)
-    tasks = JSON.parse(jsonString_a)
+    clear_screen
+    prompt = TTY::Prompt.new
+    task_choices = $tasks.map { |task| "#{task['task']} - #{task['points']} points"}
 
-    path_b = "json_tabeller/bruker.json"
-    jsonString_b = File.read(path_b)
-    users = JSON.parse(jsonString_b)
+    selected_task_choice = prompt.select("Which task was completed?".bold.yellow, task_choices, active_color: :yellow)
 
-    path_c = "json_tabeller/task_record.json"
-    jsonString_c = File.read(path_c)
-    task_records = JSON.parse(jsonString_c)
+    selected_task = $tasks.find { |task| "#{task['task']} - #{task['points']} points" == selected_task_choice}
 
+    if selected_task
+        user = $users.find { |u| u["id"] == $current_user["id"]}
+        user["points"] += selected_task["points"]
 
-    puts "Which task was completed?"
+        $task_records << {"userID" => $current_user["id"], "taskID" => selected_task["id"]}
 
-    tasks.each do | task | 
-        puts "#{task["id"]}: #{task["task"]} for #{task["points"]} points"
-    end
+        write_json("json_tabeller/bruker.json", $users)
+        write_json("json_tabeller/task_record.json", $task_records)
 
+        puts "\nLogged task: #{selected_task['task']}. You got +#{selected_task['points']} points\n"
 
-    input = gets.chomp
-    match_found = false
+        
+        print "\nRedirecting".green
+        sleep(0.7)
+        print ".".green
+        sleep(0.7)
+        print ".".green
+        sleep(1)
+        hub()
 
-    tasks.each do | task |
-        if input == task["id"]
+    else
+        print "\nERROR: No such task. Please try again..".bold.red
+        sleep(0.7)
+        print ".".bold.red
+        sleep(0.7)
+        print ".".bold.red
+        sleep(1)
 
-            update_points = users.find { |user| user["id"] == $userID }
-            update_points["points"] += task["points"]
-            File.write(path_b, JSON.dump(users))
-
-            task_records << {"userID" => $userID, "taskID" => task["id"]}
-            File.write(path_c, JSON.dump(task_records))
-
-            puts "" 
-            puts "Logged task: #{task["id"]}. You got +#{task["points"]} points"
-            puts "" 
-            match_found = true
-            hub()
-        end
-    end
-
-    unless match_found
-        puts "No such task. Please try again.."
         tasks()
     end
 end
 
-def user(name)
-    path = "json_tabeller/bruker.json"
-    jsonString = File.read(path)
-    rubyarray = JSON.parse(jsonString)
-
-    match_found = false
-    rubyarray.each do | user | 
-        if name == user["name"]
-            puts "Welcome #{user["name"]}!"
-            puts ""
-            $userID = user["id"]
-            $loop = false
-            match_found = true
-        end
-    end
-
-    unless match_found
-        puts "Did not find name, please try again.."
-        puts ""
-    end
-end
-
 def profile()
-    path_b = "json_tabeller/bruker.json"
-    jsonString_b = File.read(path_b)
-    users = JSON.parse(jsonString_b)
-
-    points = users.find { |user| user["id"] == $userID }
+    clear_screen
+    points = $users.find { |u| u["id"] == $current_user["id"] }
     
 
-    puts "-------Your Profile-------"
-    puts "Firstname: #{$user_name}"
-    puts "Points gathered: #{points["points"]}"
+    puts "-------#{$current_user["name"]}-------".bold.blue
+    puts "Firstname: #{$current_user["name"]}".green
+    puts "Points gathered: #{points["points"]}".green
 
-    puts "Go Back? (yes/no)"
+    prompt = TTY::Prompt.new
+    choice = prompt.yes?("Go back?".blue)
+        
+    if choice
+        print "\nRedirecting".green
+        sleep(0.7)
+        print ".".green
+        sleep(0.7)
+        print ".".green
+        sleep(0.7)
 
-    input = gets.chomp.upcase
-    puts ""
-
-    if input == "YES"
         hub()
+    else
+        print "\nGoodbye".green
+        sleep(0.5)
+        print "."
+        sleep(0.5)
+        print "."
+        sleep(0.5)
+        print ".#{$current_user["name"]}\n".bold.red
+        sleep(1)
+        exit
     end
-
 end
 
 def exit_program()
-
+    exit
 end 
 
 def hub()
+    clear_screen
+    prompt = TTY::Prompt.new
 
-    choices = [
-        {"id" => "1", "name" => "Check Profile", "action" => method(:profile)},
-        {"id" => "2", "name" => "Log a new task", "action" => method(:tasks)},
-        {"id" => "3", "name" => "Exit Program", "action" => method(:exit_program)}
-    ]
-
-    puts "Where do you want to go?"
-
-    choices.each do | choice |
-        puts "#{choice["id"]}: #{choice["name"]}"
-    end 
-
-    puts "(Type the number)"
-    input = gets.chomp
-    puts ""
-
-    match_found = false
-    choices.each do | choice |
-        if input == choice["id"]
-            puts ""
-            match_found = true
-            choice["action"].call
-        end
+    choice = prompt.select("Where do you want to go?".bold.green) do |menu|
+        menu.choice "Check Profile", -> { profile }
+        menu.choice "Log a new task", -> { tasks }
+        menu.choice "Exit Program", -> { exit_program }
     end
+    choice.call
+end 
 
-    unless match_found
-        puts "Not a valid choice. Please try again.."
-        puts ""
+
+def start()
+    clear_screen
+    prompt = TTY::Prompt.new
+    user_names = $users.map { |u| u["name"]}
+
+    selected_user_name = prompt.select("Who are you?".bold.green, user_names)
+
+    selected_user = $users.find { |user| user["name"] == selected_user_name}
+    $current_user = { "id" => selected_user["id"], "name" => selected_user["name"] }
+
+    if selected_user
+        clear_screen
+        puts "Welcome to TaskMaster #{$current_user["name"]}!\n".bold.green
+        sleep(2)
         hub()
+    else
+        puts "Did not find user. Please try again..".bold.red
+        sleep(2)
+        start()
     end
-end 
-
-
-
-$loop = true
-
-while $loop == true
-    puts "Type your name"
-    $user_name = gets.chomp.capitalize
-    puts ""
-
-    user($user_name)
-end 
-
-if $loop == false
-    hub()
 end
+
+
+
+$current_user = nil
+start()
+
+
+
+
+
 
 
